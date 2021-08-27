71b529dccf0d588682212c5e23ee143504ed5b03
Franco Muñoz <franco.mz92@hotmail.com>
2021-08-27 19:47:13 -0300

    Commit inicial

---
+++ src/main/java/com/actividad_final/api_informatorio/ApiInformatorioApplication.java
@@ -0,0 +1,13 @@
+package com.actividad_final.api_informatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInformatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInformatorioApplication.class, args);
+	}
+
+}
